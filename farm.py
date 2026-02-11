31e93b9d08962e27634bd136d3404ca679b83580
Fayber <deine@email.de>
2026-02-11 17:47:55 +0100

    Changed Warn Sound File path to a relative Path

--- farm.py
+++ farm.py
@@ -8,7 +8,7 @@ from config import discord_webhook_url #Needs to be imported from a config.py as
 BASE_DIR = os.path.dirname(__file__)
 LOG_PATH = os.path.join(BASE_DIR, "FarmLog.txt") #File Name in which the logs will be saved
 
-WARN_SOUND_PATH = os.path.join(BASE_DIR,"assets","AnvilLand.wav")
+WARN_SOUND_PATH = os.path.join(BASE_DIR,"assets","AnvilLand.wav") #Set this to your Warn sound file path (Default is a relative folder with the sound in it)
 
 # ========== CONFIG ==========
 PAUSE_KEY = 320
